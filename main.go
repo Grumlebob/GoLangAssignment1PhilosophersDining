72ef87c8b0a596710cbd1154fd1e442265fa48c1
Jacob Grum <jacg@itu.dk>
2022-09-08 13:27:16 +0200

    First working fork routine edition

--- main.go
+++ main.go
@@ -22,9 +22,10 @@ type philosopher struct {
 }
 
 type fork struct {
-	inUse       bool
-	id          int
-	forkChannel chan bool //ready = true. Not ready = false /empty
+	inUse        bool
+	id           int
+	forkChannel  chan bool //fork ready = true. Not ready = false /empty
+	forkRunAgain chan bool //Search for fork
 }
 
 func main() {
@@ -34,7 +35,9 @@ func main() {
 		forks[i].id = i
 		forks[i].inUse = false
 		forks[i].forkChannel = make(chan (bool), 1)
+		forks[i].forkRunAgain = make(chan (bool), 1)
 		forks[i].forkChannel <- true
+		forks[i].forkRunAgain <- true
 	}
 	//create philosophers
 	philos := make([]philosopher, numOfPhilosophers)
@@ -95,39 +98,52 @@ func main() {
 		*/
 		// NEW ROUTINE, WITH FORK ROUTINE.
 		select {
+		case <-forks[0].forkRunAgain:
+			philoPointer := &philos[0]
+			go philoPointer.goRoutineForForks()
+		case <-forks[1].forkRunAgain:
+			philoPointer := &philos[1]
+			go philoPointer.goRoutineForForks()
+		case <-forks[2].forkRunAgain:
+			philoPointer := &philos[2]
+			go philoPointer.goRoutineForForks()
+		case <-forks[3].forkRunAgain:
+			philoPointer := &philos[3]
+			go philoPointer.goRoutineForForks()
+		case <-forks[4].forkRunAgain:
+			philoPointer := &philos[4]
+			go philoPointer.goRoutineForForks()
 		case <-philos[0].philoChannel:
 			philoPointer0 := &philos[0]
-			go philoPointer0.goRoutineForForks()
 			go func() {
 				time.Sleep(1111 * time.Millisecond)
 				philoPointer0.attemptToGetForks()
 			}()
 		case <-philos[1].philoChannel:
 			philoPointer1 := &philos[1]
-			go philoPointer1.goRoutineForForks()
 			go func() {
 				time.Sleep(789 * time.Millisecond)
+				philoPointer1.attemptToGetForks()
 			}()
 		case <-philos[2].philoChannel:
 			philoPointer2 := &philos[2]
-			go philoPointer2.goRoutineForForks()
 			go func() {
 				time.Sleep(653 * time.Millisecond)
+				philoPointer2.attemptToGetForks()
 			}()
 		case <-philos[3].philoChannel:
 			philoPointer3 := &philos[3]
-			go philoPointer3.goRoutineForForks()
 			go func() {
 				time.Sleep(526 * time.Millisecond)
+				philoPointer3.attemptToGetForks()
 			}()
 		case <-philos[4].philoChannel:
 			philoPointer4 := &philos[4]
-			go philoPointer4.goRoutineForForks()
 			go func() {
 				time.Sleep(367 * time.Millisecond)
+				philoPointer4.attemptToGetForks()
 			}()
 		}
-
 	}
 	fmt.Println("Everyone is full and ate:", timesNeededToEat, " times")
 }
@@ -181,16 +197,17 @@ func (p *philosopher) goRoutineForForks() {
 		(*p).ownFork.forkChannel <- true
 		(*p).philoChannel <- (*p).id
 	case <-waitingTime: //Own fork ikke ready - prøv igen.
-		(*p).goRoutineForForks()
+		(*p).ownFork.forkRunAgain <- true
 	}
+
 }
 
 func (p *philosopher) attemptToGetForks() {
 	//KIGGER PÅ SIDEMANDS KNIV. HVIS LEDIG SÅ SPIS. ELLERS TÆNK OG LÆG EGEN KNIV LEDIG.
-	<-(*p).ownFork.forkChannel
+	//<-(*p).ownFork.forkChannel
 	waitingTime := make(chan bool, 1)
 	go func() {
-		randomPause(1)
+		randomPause(2)
 		waitingTime <- true
 	}()
 	//Vi holder egen fork i op til 1 sek.
@@ -199,10 +216,11 @@ func (p *philosopher) attemptToGetForks() {
 	//Hvis ikke så lægger vi den ned. Og begynder at tænke, mens andre kan snuppe.
 	select {
 	case <-(*p).rightFork.forkChannel: //Hvis vores nabo er ledige
-		p.eat()
-		p.returnForks() //p eller (*p) her?
+		<-(*p).ownFork.forkChannel //tag begge knive
+		(*p).eat()
+		(*p).returnForks() //p eller (*p) her?
 	case <-waitingTime: //hvis nabo fork ikke er ledige, så tænker vi
-		p.think()
+		(*p).think()
 		(*p).ownFork.forkChannel <- true
 	}
 
@@ -211,14 +229,19 @@ func (p *philosopher) attemptToGetForks() {
 func (p *philosopher) returnForks() {
 	(*p).ownFork.forkChannel <- true
 	(*p).rightFork.forkChannel <- true
-	(*p).philoChannel <- (*p).id //Indikere philo kan tage et valg. Prøv at få ind i fork routine.
+	(*p).ownFork.forkRunAgain <- true
+	(*p).rightFork.forkRunAgain <- true
 }
 
 func (p *philosopher) eat() {
+	if (*p).doneEating {
+		(*p).returnForks()
+		return
+	}
 	(*p).isThinking = false
 	(*p).timesEaten++
 	fmt.Println("philo: ", (*p).id, " is eating. Times eaten:", (*p).timesEaten)
-	if (*p).timesEaten == 3 {
+	if (*p).timesEaten >= 3 {
 		(*p).doneEating = true
 		fmt.Println("philo: ", (*p).id, " is is full of food")
 	}

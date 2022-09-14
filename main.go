ba99f7ecabbb121de8a8567c774e7c872b0da62e
Jacob Grum <jacg@itu.dk>
2022-09-14 19:58:40 +0200

    Fixed bugs, and wrote assignment comments

--- main.go
+++ main.go
@@ -7,8 +7,7 @@ import (
 )
 
 const numOfPhilosophers = 5
-const timesNeededToEat = 3
-const eatingTime = 2
+const timesNeededToEat = 100
 
 type philosopher struct {
 	philoChannel chan int
@@ -25,6 +24,23 @@ type fork struct {
 	forkRunAgain chan bool //Repeat search for own fork
 }
 
+/*
+Why it doesn't deadlock:
+Each channel only has a limit of 1 msg.
+Each fork only sends 1 msg at max, meaning if that fork gets picked up,
+it won't be able to send a message, aswell as being kept by
+only a single philosopher, due to him also having only a single channel per philosopher with a limit of 1.
+Due to passing forks by reference, we ensure we are talking to 1 channel per fork,
+and same as philosophers.
+
+The expected results are intertwined, aswell as only a max of 2 philosophers eating concurrently, due to
+the limit of 5 forks.
+
+The expected output is different each time, due to a random time delay, to simulate it taking time to pick up a fork
+and even longer to eat.
+
+We expect the program to terminate, when every philosopher has eaten %timesNeededToEat% times.
+*/
 func main() {
 	//Create forks
 	forks := make([]fork, numOfPhilosophers)
@@ -43,16 +59,21 @@ func main() {
 		philos[j].timesEaten = 0
 		philos[j].doneEating = false
 		philos[j].philoChannel = make(chan (int), 1)
-		philos[j].philoChannel <- j
 	}
 
 	//Repeat routine.
 	for {
 		//If everyone ate 3 times exit loop
-		if philos[0].doneEating && philos[1].doneEating && philos[2].doneEating && philos[3].doneEating && philos[4].doneEating {
+		if philos[0].doneEating &&
+			philos[1].doneEating &&
+			philos[2].doneEating &&
+			philos[3].doneEating &&
+			philos[4].doneEating {
 			break
 		}
 		//Start Fork and Philo routines. If fork is ready, signal philosopher.
+		//Explictly not in loop, to make it visually easy to see that
+		//every fork and philosopher has their own go routine.
 		select {
 		case <-forks[0].forkRunAgain:
 			philoPointer := &philos[0]
@@ -71,45 +92,32 @@ func main() {
 			go philoPointer.goRoutineForForks()
 		case <-philos[0].philoChannel:
 			philoPointer0 := &philos[0]
-			go func() {
-				time.Sleep(413 * time.Millisecond)
-				philoPointer0.GoRoutinePhiloEatOrThink()
-			}()
+			go philoPointer0.GoRoutinePhiloEatOrThink()
 		case <-philos[1].philoChannel:
 			philoPointer1 := &philos[1]
-			go func() {
-				time.Sleep(348 * time.Millisecond)
-				philoPointer1.GoRoutinePhiloEatOrThink()
-			}()
+			go philoPointer1.GoRoutinePhiloEatOrThink()
 		case <-philos[2].philoChannel:
 			philoPointer2 := &philos[2]
-			go func() {
-				time.Sleep(175 * time.Millisecond)
-				philoPointer2.GoRoutinePhiloEatOrThink()
-			}()
+			go philoPointer2.GoRoutinePhiloEatOrThink()
 		case <-philos[3].philoChannel:
 			philoPointer3 := &philos[3]
-			go func() {
-				time.Sleep(526 * time.Millisecond)
-				philoPointer3.GoRoutinePhiloEatOrThink()
-			}()
+			go philoPointer3.GoRoutinePhiloEatOrThink()
 		case <-philos[4].philoChannel:
 			philoPointer4 := &philos[4]
-			go func() {
-				time.Sleep(367 * time.Millisecond)
-				philoPointer4.GoRoutinePhiloEatOrThink()
-			}()
+			go philoPointer4.GoRoutinePhiloEatOrThink()
 		}
 	}
 	fmt.Println("Everyone is full and ate:", timesNeededToEat, "times")
 }
 
-func randomPause(max int) { //Fra https://github.com/iokhamafe/Golang/blob/master/diningphilosophers.go
-	time.Sleep(time.Millisecond * time.Duration(rand.Intn(max*1000)+50))
+// Pause X * 0-40 milliseconds
+func randomPause(max int) {
+	time.Sleep(time.Millisecond * time.Duration(rand.Intn(max*40)))
 }
 
 func (p *philosopher) goRoutineForForks() {
 	waitingTime := make(chan bool, 1)
+	//Own go routine, so "waiting for waitingTime isn't itself blocked"
 	go func() {
 		randomPause(1)
 		waitingTime <- true
@@ -117,7 +125,6 @@ func (p *philosopher) goRoutineForForks() {
 
 	select {
 	case <-(*p).ownFork.forkChannel: //Own fork is ready - announce to philosopher to attempt to eat.
-		(*p).ownFork.forkChannel <- true
 		(*p).philoChannel <- (*p).id
 	case <-waitingTime: //Own fork not ready, try again.
 		(*p).ownFork.forkRunAgain <- true
@@ -127,19 +134,20 @@ func (p *philosopher) goRoutineForForks() {
 
 func (p *philosopher) GoRoutinePhiloEatOrThink() {
 	waitingTime := make(chan bool, 1)
+	//Own go routine, so "waiting for waitingTime isn't itself blocked"
 	go func() {
-		randomPause(2)
+		randomPause(3)
 		waitingTime <- true
 	}()
 	//We got a channel msg that our own fork is ready, therefore check if other fork is ready, if so eat otherwise think.
 	select {
-	case <-(*p).rightFork.forkChannel:
-		<-(*p).ownFork.forkChannel //pickup both forks.
+	case <-(*p).rightFork.forkChannel: //We grab the other fork
 		(*p).eat()
 		(*p).returnForks()
-	case <-waitingTime:
+	case <-waitingTime: //They put their own fork back.
 		(*p).think()
 		(*p).ownFork.forkChannel <- true
+		(*p).ownFork.forkRunAgain <- true
 	}
 }
 
@@ -152,7 +160,7 @@ func (p *philosopher) returnForks() {
 }
 
 func (p *philosopher) eat() {
-	//If we ate 3 times, just return.
+	//If we ate 3 times, just return, otherwise print eat and status.
 	if (*p).doneEating {
 		return
 	}
@@ -163,14 +171,15 @@ func (p *philosopher) eat() {
 		(*p).doneEating = true
 		fmt.Println("philo: ", (*p).id, " is full of food")
 	}
-	randomPause(eatingTime)
+	//It takes some time to eat, wherein indirectly the keep both forks unavaible for other philosophers.
+	randomPause(2)
 	fmt.Println("philo: ", (*p).id, " is done eating")
 }
 
 func (p *philosopher) think() {
-	//Only print thinking once, untill next time we eat.
+	//Only print thinking once, until next time we eat.
 	if !(*p).isThinking {
-		fmt.Println("philo: ", (*p).id, " is now thinking untill next bite.")
+		fmt.Println("philo: ", (*p).id, " is now thinking until next bite.")
 	}
 	(*p).isThinking = true
 }
